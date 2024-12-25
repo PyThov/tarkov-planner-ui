9980bf7bf8901b15f2dba642c64c851d1cea6eed
PyThov <noahthovson@gmail.com>
2024-12-25 10:14:15 -0700

    update to build correctly for backend serving

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 3000,
     strictPort: true,
   },
   plugins: [react()],
+  base: "/static/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
 });
